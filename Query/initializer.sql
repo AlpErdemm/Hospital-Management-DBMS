af181d4cdb66d1973663a745aa07cf8f967e4b3c
osmannkartall <okartal3434@gmail.com>
2021-06-21 11:03:42 +0300

    first trigger

--- Query/initializer.sql
+++ Query/initializer.sql
@@ -24,37 +24,6 @@ create table hospital.treatment (
 	primary key (treatment_id)
 );
 
-create table hospital.patient (
-	patient_id      int,
-	identity_number    int,
-	patient_name  varchar(20) not null,
-    sex  varchar(20) not null,
-    address_id int,
-    contact_no int,
-    companion_no int,
-    admit_date varchar(20) not null,
-    discard_date varchar(20) not null,
-    room_id int,
-    treatment_id int,
-    primary key (patient_id),
-	foreign key (address_id) references address(address_id)
-		on delete set null,
-	foreign key (room_id) references room(room_id)
-		on delete set null,
-	foreign key (treatment_id) references treatment(treatment_id)
-		on delete set null
-);
-
-create table hospital.appointment (
-	appointment_id      int,
-	description    varchar(100) not null,
-	datetime  varchar(20) not null,
-	patient_id  int,
-	primary key (appointment_id),
-	foreign key (patient_id) references patient(patient_id)
-		on delete set null
-);
-
 create table hospital.employee (
 	employee_id      int,
 	contact_no int,
@@ -67,22 +36,53 @@ create table hospital.employee (
 		on delete set null
 );
 
+create table hospital.doctor (
+	doctor_id int, 
+	employee_id int,
+    position varchar(20) not null,
+    primary key (doctor_id),
+	foreign key (employee_id) references employee(employee_id)
+    on delete set null
+);
+
+create table hospital.patient (
+	patient_id      int,
+	identity_number    int,
+	patient_name  varchar(20) not null,
+    sex  varchar(20) not null,
+    address_id int,
+    contact_no int,
+    companion_no int,
+    admit_date varchar(20) not null,
+    discard_date varchar(20) not null,
+    room_id int,
+    treatment_id int,
+    doctor_id int,
+    primary key (patient_id),
+	foreign key (address_id) references address(address_id)
+		on delete set null,
+	foreign key (room_id) references room(room_id)
+		on delete set null,
+	foreign key (treatment_id) references treatment(treatment_id)
+		on delete set null,
+	foreign key (doctor_id) references doctor(doctor_id)
+  		on delete set null
+);
+
+create table hospital.appointment (
+	appointment_id      int,
+	description    varchar(100) not null,
+	datetime  varchar(20) not null,
+	patient_id  int,
+	primary key (appointment_id),
+	foreign key (patient_id) references patient(patient_id)
+		on delete set null
+);
+
 create table hospital.medicine (
 	medicine_id int,
 	price int,
     primary key (medicine_id)
-);
-
-create table hospital.doctor (
-	doctor_id int, 
-	employee_id int,
-	patient_id int,
-    position varchar(20) not null,
-    primary key (doctor_id),
-	foreign key (patient_id) references patient(patient_id)
-		on delete set null,
-	foreign key (employee_id) references employee(employee_id)
-    on delete set null
 );
 
 create table hospital.secretary (
---
+++ Query/triggers.sql
@@ -0,0 +1,34 @@
+# After running any trigger, you may need to re-initialize records in db.
+# i.e. insert old records again after running a delete operation and trigger.
+
+DELIMITER $$
+create trigger hospital.delete_doctor before delete on hospital.doctor
+for each row
+	begin
+		set @address_id = (select address_id from hospital.employee where old.employee_id = hospital.employee.employee_id);
+		# remove address of the doctor first
+		delete from hospital.address where @address_id=hospital.address.address_id;
+		# remove employee record belongs the doctor
+		delete from hospital.employee where old.employee_id = hospital.employee.employee_id;
+	end$$
+DELIMITER ;
+
+# Test: delete_doctor
+select * from hospital.doctor;
+select * from hospital.employee;
+select * from hospital.address;
+select * from hospital.patient;
+delete from hospital.doctor where doctor_id=200;
+# Note that address of the doctor is removed and value of the doctor_id attribute becomes null.  
+select * from hospital.doctor;
+select * from hospital.employee;
+select * from hospital.address;
+select * from hospital.patient;
+
+DELIMITER $$
+create trigger hospital.assing_room before insert on hospital.patient
+for each row
+	begin
+		# bos oda yoksa -1 ata, diger turlu buldugun ilk bos odayi ata.
+	end$$
+DELIMITER ;
